0eb36f08987931856f81dc262b73f4e327bea2b5
Nicolas-Saavedra <personal@nicolassaavedra.com>
2025-05-07 18:53:56 -0500

    feat: added tables for missing entities

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,32 @@
-import { pgTable, varchar } from "drizzle-orm/pg-core";
+import {
+  doublePrecision,
+  numeric,
+  pgTable,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-  email: varchar({ length: 127 }).notNull().unique(),
-  secret: varchar({ length: 127 }).notNull().unique(),
+export const users = pgTable("users", {
+  email: varchar({ length: 64 }).notNull().unique(),
+  secret: varchar({ length: 128 }).notNull().unique(),
+});
+
+export const transactions = pgTable("transactions", {
+  id: uuid("id").primaryKey().notNull(),
+  transactionType: varchar("transaction_type", { length: 32 }).notNull(),
+  transactionDescription: varchar("transaction_description", {
+    length: 255,
+  }).notNull(),
+  amount: numeric("amount").notNull(),
+  currencyCode: varchar("currency_code", { length: 3 }).notNull(), // ISO 4217 codes are 3 characters
+});
+
+export const restaurants = pgTable("restaurants", {
+  id: varchar("id", { length: 127 }).primaryKey().notNull(),
+  name: varchar("name", { length: 255 }).notNull(),
+  streetAddress: varchar("street_address", { length: 255 }).notNull(),
+  city: varchar("city", { length: 127 }).notNull(),
+  country: varchar("country", { length: 127 }).notNull(),
+  latitude: doublePrecision("latitude").notNull(),
+  longitude: doublePrecision("longitude").notNull(),
 });
